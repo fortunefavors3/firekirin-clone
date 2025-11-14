35eec1831da811e0824b92fe0794132f364ac808
fortunefavors3 <fortunefavors3@gmail.com>
2025-11-14 02:06:00 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# FireKirin Clone Starter
+
+This is a starter template with a React Native client and an Express server.
+
+## Run Instructions
+
+### Server
+```
+cd server
+npm install
+npm run dev
+```
+
+### Client
+```
+cd client
+npm install
+npm start
+```
---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'FireKirin Clone Server Running' });
+});
+
+app.listen(4000, () => console.log('Server running on port 4000'));
